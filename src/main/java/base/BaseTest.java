8d04b96397c915a7a138624661389eef1de676ca
Sarvesh1709 <sarveshdlatkar@gmail.com>
2025-04-18 21:38:46 +0530

    adding new files

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -1,21 +1,48 @@
 package base;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import utils.ExtentReportManager;
+import utils.Log;
+
 
 public class BaseTest {
     protected WebDriver driver;
+    protected static ExtentReports extent;
+    protected ExtentTest test;
+    @BeforeSuite
+    public void setupReport(){
+       extent = ExtentReportManager.getReportInstance();
+    }
+    @AfterSuite
+    public void teardownReport(){
+       extent.flush();
+    }
     @BeforeMethod
     public void setUp(){
+        Log.info("Starting WebDriver...");
         driver=new ChromeDriver();
         driver.manage().window().maximize();
+        Log.info("Navigating to URL...");
         driver.get("https://admin-demo.nopcommerce.com/login");
     }
     @AfterMethod
-    public void tearDown(){
+    public void tearDown(ITestResult result){
+        if (result.getStatus()==ITestResult.FAILURE){
+            String screenshotPath = ExtentReportManager.captureScreenShot(driver,"LoginFailure");
+            test.fail("Test Failed.. Check Screenshot",
+                    MediaEntityBuilder.createScreenCaptureFromPath(screenshotPath).build());
+        }
         if (driver !=null){
+            Log.info("Closing Browser");
             driver.quit();
         }
     }
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -5,15 +5,48 @@ import org.openqa.selenium.internal.Debug;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.LoginPage;
+import utils.ExtentReportManager;
+import utils.Log;
 
 public class LoginTest extends BaseTest {
     @Test
-   public void testValidLogin(){
-       LoginPage loginPage=new LoginPage(driver);
-       loginPage.enterPassowrd("admin@yourstore.com");
-       loginPage.enterPassowrd("admin");
-       loginPage.clickLogin();
-       System.out.println("Title of the page is:"+driver.getTitle());
-        Assert.assertEquals(driver.getTitle(),"Just a moment...");
-   }
+    public void testValidLogin() {
+        Log.info("Starting the login test..");
+        test = ExtentReportManager.createTest("Login Test");
+
+        test.info("Navigating to URL");
+        LoginPage loginPage = new LoginPage(driver);
+
+        Log.info("Adding credentials");
+        test.info("Adding Credentials");
+        loginPage.enterPassowrd("admin@yourstore.com");
+        loginPage.enterPassowrd("admin");
+        loginPage.clickLogin();
+
+        System.out.println("Title of the page is:" + driver.getTitle());
+        Log.info("Verifying page title");
+        test.info("Verifying page title");
+        Assert.assertEquals(driver.getTitle(), "Just a moment...");
+        test.pass("Login Successful");
+    }
+    @Test
+    public void testLoginWithInvalidCredentials() {
+        Log.info("Starting the login test..");
+        test = ExtentReportManager.createTest("Login Test with Invalid Credentials");
+
+        test.info("Navigating to URL");
+        LoginPage loginPage = new LoginPage(driver);
+
+        Log.info("Adding credentials");
+        test.info("Adding Credentials");
+        loginPage.enterPassowrd("admin1234@yourstore.com");
+        loginPage.enterPassowrd("admin123");
+        loginPage.clickLogin();
+
+        System.out.println("Title of the page is:" + driver.getTitle());
+        Log.info("Verifying page title");
+        test.info("Verifying page title");
+        Assert.assertEquals(driver.getTitle(), "Just a moment...");
+        test.pass("Login Successful");
+    }
 }
